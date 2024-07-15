7c989c409a8e0ff96b37aa8c88196ec403b94f4e
Lucy <rafploooz@gmail.com>
2024-07-15 03:35:42 -0400

    basic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::sync::Mutex;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+struct StateCounter {
+    counter: Mutex<i32>,
+}
+
+async fn hello(data: web::Data<StateCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("hi! you are #{counter}")
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    let counter = web::Data::new(StateCounter {
+        counter: Mutex::new(0),
+    });
+    HttpServer::new(move || {
+        App::new()
+            .app_data(counter.clone())
+            .route("/", web::get().to(hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }

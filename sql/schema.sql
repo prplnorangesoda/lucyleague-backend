9fece21d63b3fc8f5cdbd9b14af084dcbdfc86d1
Lucy Faria <rafploooz@gmail.com>
2024-07-26 00:06:51 -0400

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,9 +3,19 @@ CREATE SCHEMA testing;
 
 CREATE TABLE testing.users (
 	id BIGSERIAL PRIMARY KEY,
-	email VARCHAR(200) NOT NULL,
-	first_name VARCHAR(200) NOT NULL,
-	last_name VARCHAR(200) NOT NULL,
+	steamid BIGINT,
 	username VARCHAR(50) UNIQUE NOT NULL,
 	UNIQUE (username)
-)
+);
+
+CREATE TABLE testing.teams (
+	id BIGSERIAL PRIMARY KEY,
+	team_name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE testing.userTeam (
+	userid BIGSERIAL NOT NULL,
+	teamid BIGSERIAL NOT NULL,
+	CONSTRAINT FK_userTeam_user FOREIGN KEY (userid) references testing.users(id),
+	CONSTRAINT FK_userTeam_team FOREIGN KEY (teamid) references testing.teams(id)
+);
